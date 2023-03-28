2fcf5ac3690efbee96e65f8b7af2cc1d6339ea08
AngusSAE <99470116+AngusSAE@users.noreply.github.com>
2023-03-28 14:31:42 +1000

    Made player and trees spawn and movable
    can still go through trees and don't have rotations

--- Assets/Scripts/WorldSpawner.cs
+++ Assets/Scripts/WorldSpawner.cs
@@ -7,7 +7,8 @@ public class WorldSpawner : MonoBehaviour
 {
     [SerializeField] protected Glyph currentGlpyh;
     [SerializeField] protected TextAsset currentLevel;
-    [SerializeField] protected Transform floorparent;
+    [SerializeField] protected Transform floorParent;
+    [SerializeField] protected Transform objectParent;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +23,9 @@ public class WorldSpawner : MonoBehaviour
         string groundText = currentLevel.text;
 
         string[] groundArray = groundText.Split('\n');
-        int layer = 0;
+        int x = 0;
         int z = 0;
+        int layer = 0;
         char[] mychars = groundText.ToCharArray();
 
         for (int i=0; i < mychars.Length; i++)
@@ -39,22 +41,47 @@ public class WorldSpawner : MonoBehaviour
 
                 else if (mychars[i] == '*')
                 {
-                    layer++;
+                    x++;
                     z = 0;
                     Debug.Log("Hello Clay");
+                }
+                else if (mychars[i] == '.')
+                {
+                    z++;
+                    Debug.Log("Hello Nathan");
                 }
 
                 else if(mychars[i] == '1')
                 {
-                    currentGlpyh.SpawnGlyphItem('1', new Vector3(layer, 0, z), 0, '.', floorparent);
+                    currentGlpyh.SpawnGlyphItem('1', new Vector3(x, 0, z), layer, '.', floorParent);
                     z++;
                     Debug.Log("Hello Bailey");
                 }
                 else if (mychars[i] == '2')
                 {
-                    currentGlpyh.SpawnGlyphItem('2', new Vector3(layer, 0, z), 0, '.', floorparent);
+                    currentGlpyh.SpawnGlyphItem('2', new Vector3(x, 0, z), layer, '.', floorParent);
                     z++;
                     Debug.Log("Hello Matt!");
+                }
+                else if (mychars[i] == '?')
+                {
+                    layer++;
+                    z = 0;
+                    x = 0;
+                    Debug.Log("Hello Greg");
+                }
+
+                else if (mychars[i] == 't')
+                {
+                    currentGlpyh.SpawnGlyphItem('t', new Vector3(x, 0, z), layer, '.', objectParent);
+                    z++;
+                    Debug.Log("Hello Nat");
+                }
+                else if (mychars[i] == 'p')
+                {
+                    currentGlpyh.SpawnGlyphItem('p', new Vector3(x, 0, z), layer, '.', objectParent);
+                    z++;
+                    Debug.Log("Hello Bridget!");
                 }
             }
         }
