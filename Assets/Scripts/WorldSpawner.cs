db98858a200cfdd37c8751c89b8f59c8f6b04a60
Angus Collins <1024762@student.sae.edu.au>
2023-04-03 15:09:08 +1000

    added rotations and fixed some code

--- Assets/Scripts/WorldSpawner.cs
+++ Assets/Scripts/WorldSpawner.cs
@@ -23,69 +23,79 @@ public class WorldSpawner : MonoBehaviour
         string groundText = currentLevel.text;
 
         string[] groundArray = groundText.Split('\n');
-        int x = 0;
+        //int x = 0;
         int z = 0;
         int layer = 0;
-        char[] mychars = groundText.ToCharArray();
-
-        for (int i=0; i < mychars.Length; i++)
+        for (int line = 0; line < groundArray.Length; ++line)
         {
-            
-            if(mychars.Length > 0)
+            char[] mychars = groundArray[line].ToCharArray();
+            int width = (mychars.Length - 2) / 2;
+            if (groundArray[line] == "?\r")
             {
-                if(mychars[0] == '/')
+                layer++;
+                z = 0;
+                continue;
+            }
+            for (int x = 0; x < width; x++)
+            {
+                char rotation = mychars[x+ width + 1]; 
+                if (mychars.Length > 0)
                 {
-                    //do nothing
-                    Debug.Log("Hello ME");
-                }
+                    if (mychars[0] == '/')
+                    {
+                        break;
+                        //do nothing
+                        Debug.Log("Hello ME");
+                    }
 
-                else if (mychars[i] == '*')
-                {
-                    x++;
-                    z = 0;
-                    Debug.Log("Hello Clay");
-                }
-                else if (mychars[i] == '.')
-                {
-                    z++;
-                    Debug.Log("Hello Nathan");
-                }
+                    else if (mychars[x] == '*')
+                    {
+                        //x++;
+                        //z = 0;
+                        Debug.Log("Hello Clay");
+                    }
+                    else if (mychars[x] == '.')
+                    {
+                        //z++;
+                        Debug.Log("Hello Nathan");
+                    }
 
-                else if(mychars[i] == '1')
-                {
-                    currentGlpyh.SpawnGlyphItem('1', new Vector3(x, 0, z), layer, '.', floorParent.transform);
-                    z++;
-                    Debug.Log("Hello Bailey");
-                }
-                else if (mychars[i] == '2')
-                {
-                    currentGlpyh.SpawnGlyphItem('2', new Vector3(x, 0, z), layer, '.', floorParent.transform);
-                    z++;
-                    Debug.Log("Hello Matt!");
-                }
-                else if (mychars[i] == '?')
-                {
-                    layer++;
-                    z = 0;
-                    x = 0;
-                    Debug.Log("Hello Greg");
-                }
+                    else if (mychars[x] == '1')
+                    {
+                        currentGlpyh.SpawnGlyphItem('1', new Vector3(x, 0, z), layer, rotation, floorParent.transform);
+                        //z++;
+                        Debug.Log("Hello Bailey");
+                    }
+                    else if (mychars[x] == '2')
+                    {
+                        currentGlpyh.SpawnGlyphItem('2', new Vector3(x, 0, z), layer, rotation, floorParent.transform);
+                        //z++;
+                        Debug.Log("Hello Matt!");
+                    }
+                    else if (mychars[x] == '?')
+                    {
+                        layer++;
+                        z = 0;
+                        //x = 0;
+                        Debug.Log("Hello Greg");
+                    }
 
-                else if (mychars[i] == 't')
-                {
-                    currentGlpyh.SpawnGlyphItem('t', new Vector3(x, 0, z), layer, '.', objectParent.transform);
-                    z++;
-                    Debug.Log("Hello Nat");
-                }
-                else if (mychars[i] == 'p')
-                {
-                    currentGlpyh.SpawnGlyphItem('p', new Vector3(x, 0, z), layer, '.', objectParent.transform);
-                    z++;
-                    Debug.Log("Hello Bridget!");
+                    else if (mychars[x] == 't')
+                    {
+                        currentGlpyh.SpawnGlyphItem('t', new Vector3(x, 0, z), layer, rotation, objectParent.transform);
+                        //z++;
+                        Debug.Log("Hello Nat");
+                    }
+                    else if (mychars[x] == 'p')
+                    {
+                        currentGlpyh.SpawnGlyphItem('p', new Vector3(x, 0, z), layer, rotation, objectParent.transform);
+                        //z++;
+                        Debug.Log("Hello Bridget!");
+                    }
                 }
             }
+            z++;
         }
-
 
 
     }
